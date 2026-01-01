88d6d7677b7e384617f077fa0fd1cc6b29e71039
ManiRaja <maniraja@Nesamanis-MacBook-Air.local>
2026-01-01 10:13:09 +0530

    Update P&L verified URL and refresh data

--- src/config.js
+++ src/config.js
@@ -1,1 +1,1 @@
-export const VERIFIED_PNL_URL = "https://verified.flattrade.in/pnl/04171ae307334a59a920969382d77a26";
+export const VERIFIED_PNL_URL = "https://wall.flattrade.in/pnl/PO549c8896286f484db6b71f387d434d9f";
